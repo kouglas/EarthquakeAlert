2358dbf06e6182ef0c270d580d8098c50136cf3c
Kari <kouglas2@gmail.com>
2025-05-30 22:06:02 -0500

    Initial Commit

---
+++ EarthquakeAlert/EarthquakeAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthquakeAlertApp.swift
+//  EarthquakeAlert
+//
+//  Created by Kari Douglas on 5/30/25.
+//
+
+import SwiftUI
+
+@main
+struct EarthquakeAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
